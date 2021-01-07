995ea3bff14a08477142383d7b8d38f8b380a522
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-07 11:47:43 +0300

    Modified DepartmentService file to include all DepartmentDao methods

--- src/main/java/components/departments/DepartmentService.java
+++ src/main/java/components/departments/DepartmentService.java
@@ -1,5 +1,38 @@
 package components.departments;
 
+import components.members.Member;
+
+import java.util.List;
+
 public class DepartmentService implements DepartmentDao{
 
+    @Override
+    public List<Department> getAllDepartments() {
+        return null;
+    }
+
+    @Override
+    public List<Member> getAllDepartmentMembers(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public void addDepartment(Department department) {
+
+    }
+
+    @Override
+    public Department getDepartmentById(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public void updateDepartment(int departmentId, String name) {
+
+    }
+
+    @Override
+    public void deleteDepartmentById(int departmentId) {
+
+    }
 }

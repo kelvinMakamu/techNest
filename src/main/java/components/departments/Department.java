2986debaa9216fb7b3cbdd3e0602289513e0bc0c
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-07 10:26:23 +0300

    Modified Department model file to enforce encapsulation, added getters and setters

--- src/main/java/components/departments/Department.java
+++ src/main/java/components/departments/Department.java
@@ -1,4 +1,53 @@
 package components.departments;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Department {
+
+    private int id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Department(String name){
+        this.name      = name;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+        Department that = (Department) o;
+        return getId() == that.getId() && getName().equals(that.getName()) && getCreatedAt().equals(that.getCreatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCreatedAt());
+    }
 }

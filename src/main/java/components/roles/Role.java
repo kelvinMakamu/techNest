191d9d169747e04fdae1e9da356058c2c945b850
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-07 11:13:42 +0300

    Modified Role model file to enforce encapsulation, add getters, setters, hashcode and equals method

--- src/main/java/components/roles/Role.java
+++ src/main/java/components/roles/Role.java
@@ -1,4 +1,52 @@
 package components.roles;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Role {
+    private int id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Role(String name){
+        this.name      = name;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return getId() == role.getId() && getName().equals(role.getName()) && getCreatedAt().equals(role.getCreatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getCreatedAt());
+    }
 }

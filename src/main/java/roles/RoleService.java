761562467275b8f9373df5ec09ccabd0b4bf5a1a
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-10 21:25:10 +0300

    Modified RoleService Class to implement all RoleDao defined methods

--- src/main/java/roles/RoleService.java
+++ src/main/java/roles/RoleService.java
@@ -1,4 +1,36 @@
 package roles;
 
-public class RoleService {
+import java.util.List;
+
+public class RoleService implements RoleDao{
+
+    @Override
+    public List<Role> getAllRoles() {
+        return null;
+    }
+
+    @Override
+    public void addRole(Role role) {
+
+    }
+
+    @Override
+    public Role getRoleById(int roleId) {
+        return null;
+    }
+
+    @Override
+    public void updateRole(int roleId, String name) {
+
+    }
+
+    @Override
+    public void deleteRoleById(int roleId) {
+
+    }
+
+    @Override
+    public void deleteRoles() {
+
+    }
 }

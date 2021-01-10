404766ecebd332a9f37b6ceabc9ba0abbf014445
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-10 21:23:16 +0300

    Modified RoleDao interface to add all methods to be implemented

--- src/main/java/roles/RoleDao.java
+++ src/main/java/roles/RoleDao.java
@@ -1,4 +1,21 @@
 package roles;
 
+import departments.Department;
+import members.Member;
+
+import java.util.List;
+
 public interface RoleDao {
+    // LIST ALL AVAILABLE ROLES
+    List<Role> getAllRoles();
+    // ADD NEW ROLE
+    void addRole(Role role);
+    // GET RESPECTIVE ROLE BY ID
+    Role getRoleById(int roleId);
+    // UPDATE RESPECTIVE ROLE
+    void updateRole(int roleId, String name);
+    // DELETE RESPECTIVE ROLE
+    void deleteRoleById(int roleId);
+    // DELETE ALL ROLES
+    void deleteRoles();
 }

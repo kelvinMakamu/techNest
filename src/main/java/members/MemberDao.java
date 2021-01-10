031023fb0f7c31abc3dfd1993930c685ff42b4a7
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-10 03:37:25 +0300

    Modified MemberDao class to add all implementable methods

--- src/main/java/members/MemberDao.java
+++ src/main/java/members/MemberDao.java
@@ -1,4 +1,20 @@
-package components.members;
+package members;
+
+import departments.Department;
+
+import java.util.List;
 
 public interface MemberDao {
+    // LIST ALL AVAILABLE MEMBERS
+    List<Member> getAllMembers();
+    // ADD NEW MEMBER
+    void addMember(Member member);
+    // GET RESPECTIVE MEMBER BY ID
+    Department getMemberById(int memberId);
+    // UPDATE RESPECTIVE MEMBER
+    void updateMember(int memberId, String firstName, String lastName, int departmentId);
+    // DELETE RESPECTIVE MEMBER
+    void deleteMemberById(int memberId);
+    // DELETE ALL MEMBERS
+    void deleteMembers();
 }

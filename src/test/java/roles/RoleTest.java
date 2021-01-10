1189bb03f4ae8b4f4d790ac67431681b49cac142
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-01-10 21:11:02 +0300

    Modified RoleTest class to add a test case for instantiating a Role

--- src/test/java/roles/RoleTest.java
+++ src/test/java/roles/RoleTest.java
@@ -1,7 +1,9 @@
 package roles;
 
+import departments.Department;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -15,5 +17,15 @@ public class RoleTest {
     public void tearDown() throws Exception {
     }
 
-    
+    @Test
+    public void NewRole_InstantiatesCorrectly_true(){
+        Role role = setupNewRole();
+        assertEquals(true, role instanceof Role);
+    }
+
+    // HELPER
+    public Role setupNewRole(){
+        return new Role("Chairman");
+    }
+
 }
